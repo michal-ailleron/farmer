933eed7fff5c9cf080e223d4662094f461c44a66
miba <michal.baraniak@ailleron.com>
2020-03-07 17:33:28 +0100

    Add player farm implementation

--- src/test/java/com/game/farmer/farm/FarmFactoryTest.java
+++ src/test/java/com/game/farmer/farm/FarmFactoryTest.java
@@ -28,10 +28,45 @@ class FarmFactoryTest {
     }
 
     @Test
-    void return_filled_box_for_main_farm() {
+    void check_farms_box_size() {
         //when
         final Farm mainFarm = farmFactory.createMainFarm();
+        final Farm playerFarm = farmFactory.createPlayerFarm();
         //then
+        assertBoxSize(mainFarm);
+        assertBoxSize(playerFarm);
+    }
+
+    @Test
+    void return_filled_box_with_animals_for_main_farm() {
+        //given
+        final Farm mainFarm = farmFactory.createMainFarm();
+        //then
+        assertEquals(farmProperties.getRabbit().getSize(), mainFarm.getRabbitBox().getAnimals().size());
+        assertEquals(farmProperties.getSheep().getSize(), mainFarm.getSheepBox().getAnimals().size());
+        assertEquals(farmProperties.getPig().getSize(), mainFarm.getPigBox().getAnimals().size());
+        assertEquals(farmProperties.getCow().getSize(), mainFarm.getCowBox().getAnimals().size());
+        assertEquals(farmProperties.getHorse().getSize(), mainFarm.getHorseBox().getAnimals().size());
+        assertEquals(farmProperties.getSmallDog().getSize(), mainFarm.getSmallDogBox().getAnimals().size());
+        assertEquals(farmProperties.getBigDog().getSize(), mainFarm.getBigDogBox().getAnimals().size());
+    }
+
+    @Test
+    void should_return_init_player_farm() {
+        //given
+        final Farm playerFarm = farmFactory.createPlayerFarm();
+        //then
+        assertEquals(0, playerFarm.getRabbitBox().getAnimals().size());
+        assertEquals(0, playerFarm.getSheepBox().getAnimals().size());
+        assertEquals(0, playerFarm.getPigBox().getAnimals().size());
+        assertEquals(0, playerFarm.getCowBox().getAnimals().size());
+        assertEquals(0, playerFarm.getHorseBox().getAnimals().size());
+        assertEquals(0, playerFarm.getSmallDogBox().getAnimals().size());
+        assertEquals(0, playerFarm.getBigDogBox().getAnimals().size());
+
+    }
+
+    private void assertBoxSize(Farm mainFarm) {
         assertEquals(farmProperties.getRabbit().getSize(), mainFarm.getRabbitBox().getSize());
         assertEquals(farmProperties.getSheep().getSize(), mainFarm.getSheepBox().getSize());
         assertEquals(farmProperties.getPig().getSize(), mainFarm.getPigBox().getSize());
