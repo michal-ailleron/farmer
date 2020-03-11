a832e0ee5c8866935e31ac2c21fcff34a5ec7ac3
miba <michal.baraniak@ailleron.com>
2020-03-11 22:08:38 +0100

    add animal exchange interface

---
+++ src/main/java/com/game/farmer/exchange/AnimalExchange.java
@@ -0,0 +1,12 @@
+package com.game.farmer.exchange;
+
+/**
+ * <h1>AnimalExchanger</h1>
+ * <br>
+ * <p/>
+ * Creation date: 08.03.2020
+ *
+ * @author miba <a href="http://www.ailleron.com">AILLERON S.A.</a>
+ **/
+public interface AnimalExchanger {
+}
---
+++ src/main/java/com/game/farmer/exchange/AnimalExchangeImpl.java
@@ -0,0 +1,12 @@
+package com.game.farmer.exchange;
+
+/**
+ * <h1>AnimalExchangeImpl</h1>
+ * <br>
+ * <p/>
+ * Creation date: 11.03.2020
+ *
+ * @author miba <a href="http://www.ailleron.com">AILLERON S.A.</a>
+ **/
+public class AnimalExchangeImpl {
+}
